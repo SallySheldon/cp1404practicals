b40a6819376ea950c2ac6dce31e03cbe1e0ebc0d
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-09 22:11:41 +1000

    Remove redundant variable in evaluate_result(); introduce prompt parameter in get_valid_number()

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -10,7 +10,7 @@ from random import randint
 def main():
     """Program to generate and evaluate a user-inputted number of random
     scores and output results to file."""
-    number_of_scores = get_valid_number()
+    number_of_scores = get_valid_number("Enter number of scores: ")
     out_file = open("results.txt", "w")
     for i in range(number_of_scores):
         score = randint(0, 100)
@@ -19,26 +19,25 @@ def main():
     out_file.close()
 
 
-def get_valid_number():
+def get_valid_number(prompt):
     """Prompt user for a number between 0 and 100."""
-    number = int(input("Enter number: "))
+    number = int(input(prompt))
     while number < 0 or number > 100:
         print("Number must be between 0 and 100.")
-        number = int(input("Enter number: "))
+        number = int(input(prompt))
     return number
 
 
 def evaluate_result(score):
     """Evaluate the result from a given score."""
     if score < 0 or score > 100:
-        result = "Invalid score"
+        return "Invalid score"
     elif score >= 90:
-        result = "Excellent"
+        return "Excellent"
     elif score >= 50:
-        result = "Passable"
+        return "Passable"
     else:
-        result = "Bad"
-    return result
+        return "Bad"
 
 
 main()
