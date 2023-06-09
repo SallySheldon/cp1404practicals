a9ae971392bc6219e7d51ed27b5dae6c74b70a45
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-09 21:17:31 +1000

    Sharpen comments, names, prompts after reviewing prac_02 solutions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,28 +3,29 @@ CP1404 Prac 02
 First exercise - Password stars program - Practise committing to Git
 """
 
-DEFAULT_MINIMUM_LENGTH = 8
+MINIMUM_LENGTH = 8
 
 
 def main():
-    """Program to ask user to set a password meeting minimum length requirements
-    then print asterisks as long as the password."""
-    password = get_valid_password(DEFAULT_MINIMUM_LENGTH)
+    """Program to set a password meeting minimum length requirements
+    then print as many asterisks as the password has characters."""
+    password = get_valid_password(MINIMUM_LENGTH)
     print_asterisks(password)
 
 
 def get_valid_password(minimum_length):
-    """Prompt user for password that meets minimum length requirements."""
-    password = input("Set password: ")
+    """Get password that meets minimum length requirements."""
+    password = input(f"Set password (minimum of {minimum_length} characters): ")
     while len(password) < minimum_length:
         print(f"Password must be a minimum of {minimum_length} characters.")
-        password = input("Password: ")
+        password = input(
+            f"Set password (minimum of {minimum_length} characters): ")
     return password
 
 
-def print_asterisks(password):
-    """Print asterisks as long as a supplied password."""
-    print("*" * len(password))
+def print_asterisks(sequence):
+    """Print as many asterisks as there are characters in supplied sequence."""
+    print("*" * len(sequence))
 
 
 main()
