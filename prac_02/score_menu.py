d6fdc11edc8ac51f2fb7b2afa6f3e8090b8bac69
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-09 14:32:17 +1000

    Add menu-driven score evaluation program reusing functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,60 @@
+"""
+CP1404 -
+Practical 02 - Do-from-scratch exercises
+Menu
+"""
+
+MENU = """(G)et a valid score (must be 0-100 inclusive)
+(P)rint result
+(S)how stars
+(Q)uit
+"""
+
+
+def main():
+    """Menu-driven program to evaluate a user-inputted score and print
+    that many stars."""
+    score = get_valid_score()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = evaluate_result(score)
+            print(f"A score of {score} is {result}\n")
+        else:
+            print_asterisks(score)
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell!")
+
+
+def get_valid_score():
+    """Prompt user for a score between 0 and 100."""
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Score must be between 0 and 100.")
+        score = int(input("Enter score: "))
+    return score
+
+
+def evaluate_result(score):
+    """Evaluate the result from a given score."""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+def print_asterisks(number):
+    """Print as many asterisks as a supplied number."""
+    print("*" * number)
+
+
+main()
