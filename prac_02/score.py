bcd387416e157ce5c8588f03e2a1f93cf6b6ee6e
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-09 10:09:03 +1000

    Refactor score program using result evaluating function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,28 @@
 """
-CP1404 - Practical 01
+CP1404 -
+Practical 02 reworking of Prac 01 program using functions
 Fix broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """Program to ask a user for their score and print the evaluated result."""
+    score = float(input("Enter score: "))
+    result = evaluate_result(score)
+    print(result)
+
+
+def evaluate_result(score):
+    """Evaluate the result from a given score."""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
