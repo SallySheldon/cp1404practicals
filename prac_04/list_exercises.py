f219cda0c5868d04b5c1a4f0be0139fd50d07367
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-20 15:47:14 +1000

    Write program to check whether username is in a list of authorised users

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,7 +4,6 @@ Intermediate Exercises
 """
 
 # Basic list operations
-
 NUMBER_OF_NUMBERS = 5
 
 numbers = []
@@ -21,3 +20,14 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {(sum(numbers) / NUMBER_OF_NUMBERS):.1f}")
+
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                        'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+                        'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
