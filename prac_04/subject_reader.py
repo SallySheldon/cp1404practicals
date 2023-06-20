d02a8aeaa7a3730a0775d789b5ce660ddd03c77e
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-20 14:30:51 +1000

    Incorporate alignments in function to display subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -18,11 +18,11 @@ def get_data():
     input_file = open(FILENAME)
     data = []
     for line in input_file:
-        # print(line)  # See what a line looks like
-        # print(repr(line))  # See what a line really looks like
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
+        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
@@ -32,9 +32,14 @@ def get_data():
 
 
 def display_subject_details(data):
-    """Display subject details from a nested list in sentence form."""
+    """Display subject details from a nested list in neatly aligned sentence form."""
+    # Use length of longest strings for subject code, teacher name and student numbers, as alignment spacing.
+    code_alignment = max((len(record[0]) for record in data))
+    name_alignment = max((len(record[1]) for record in data))
+    number_alignment = max((len(str(record[2])) for record in data))
     for record in data:
-        print(f"{record[0]} is taught by {record[1]} and has {record[2]} students")
+        print(f"{record[0]:{code_alignment}} is taught by {record[1]:{name_alignment}} and has "
+              f"{record[2]:{number_alignment}} students")
 
 
 main()
