e79af1b0a02904a2f688083cda341f271bb17968
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-23 20:34:11 +1000

    Complete print expressions for required wimbledon.py output; tidy code

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -16,25 +16,11 @@ def main():
     results = process_file(filename)
     champion_to_wins = store_champion_wins(results)
     countries = store_countries(results)
-    print(champion_to_wins)
-    print(countries)
-
-
-def store_countries(results):
-    """Store the countries of the champions in a set."""
-    countries = {result[1] for result in results}
-    return countries
-
-
-def store_champion_wins(results):
-    """Store the number of wins per champion in a dictionary."""
-    champion_to_wins = {}
-    for result in results:
-        try:
-            champion_to_wins[result[2]] += 1
-        except KeyError:
-            champion_to_wins[result[2]] = 1
-    return champion_to_wins
+    print("Wimbledon Champions:")
+    for champion, wins in champion_to_wins.items():
+        print(champion, wins)
+    print(f"\nThese {len(countries)} countries have won Wimbledon:")
+    print(', '.join(sorted(countries)))
 
 
 def process_file(filename):
@@ -49,5 +35,21 @@ def process_file(filename):
     return results
 
 
-main()
+def store_champion_wins(results):
+    """Store the number of wins per champion in a dictionary."""
+    champion_to_wins = {}
+    for result in results:
+        try:
+            champion_to_wins[result[2]] += 1
+        except KeyError:
+            champion_to_wins[result[2]] = 1
+    return champion_to_wins
 
+
+def store_countries(results):
+    """Store the countries of the champions in a set."""
+    countries = {result[1] for result in results}
+    return countries
+
+
+main()
