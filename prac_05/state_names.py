97ab14abc6ec9cf505779e06bf881a4f484e8b97
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-23 12:00:36 +1000

    Complete prac 5 state_names.py exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,26 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    # LBYL approach:
+    # if state_code in CODE_TO_NAME:
+    #     print(state_code, "is", CODE_TO_NAME[state_code])
+    # else:
+    #     print("Invalid short state")
+
+    # EAFP approach:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+print()  # Print blank line after user exits input mode.
+
+maximum_width = max((len(code) for code in CODE_TO_NAME))
+for code in CODE_TO_NAME:
+    print(f"{code:<{maximum_width}} is {CODE_TO_NAME[code]}")
