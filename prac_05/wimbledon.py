cd5023f6b0ad68a11001ecb10c9e57982812caaa
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-23 19:38:18 +1000

    Refactor process file steps as separate function in wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -10,21 +10,32 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
-    """Program to read file of Wimbledon men's final results and report champions' nationality and number of wins."""
+    """Program to read file of Wimbledon men's final results and display champions' nationalities and number of wins."""
     filename = FILENAME
-    with open(filename, 'r', encoding='utf-8-sig') as in_file:
-        data = in_file.read().split('\n')[1:]  # Split data by year (at \n), removing header row
+    results = process_file(filename)
     champion_to_number = {}
     # Store champion's name (key) and number of times won (value) in dictionary
-    for record in data:
+    for result in results:
         try:
-            champion_to_number[record.split(',')[2]] += 1
+            champion_to_number[result.split(',')[2]] += 1
         except KeyError:
-            champion_to_number[record.split(',')[2]] = 1
+            champion_to_number[result.split(',')[2]] = 1
     print(champion_to_number)
     # Store champions' nationalities in set
-    nationalities = {record.split(',')[1] for record in data}
+    nationalities = {result.split(',')[1] for result in results}
     print(nationalities)
+
+
+def process_file(filename):
+    """Extract annual Wimbledon champion and nationality details from a supplied file and return them as a nested
+    list."""
+    with open(filename, 'r', encoding='utf-8-sig') as in_file:
+        in_file.readline()  # Ignore header row
+        results = []
+        for line in in_file:
+            result = line.strip().split(',')[:3]  # Extract year, country, champion for each annual result
+            results.append(result)  # Store annual results in nested list
+    return results
 
 
 main()
