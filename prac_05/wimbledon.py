9c0b73c31afa71d08dcfb8e48518fa13dd6f103a
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-06-27 22:06:27 +1000

    Remove unnecessary filename variable, use constants for index values, in wimbledon.py (after reviewing solutions)

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,13 +7,14 @@ Actual:     65 minutes
 """
 
 FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
 
 
 def main():
-    """Program to get and display the names of the men's Wimbledon singles champions, how many times they have
-    won, and their countries."""
-    filename = FILENAME
-    results = process_file(filename)
+    """Program to read from data file and display the names of the men's Wimbledon singles champions, how many times
+    they have won, and their countries."""
+    results = process_file(FILENAME)
     champion_to_wins = store_champion_wins(results)
     countries = store_countries(results)
     print("Wimbledon Champions:")
@@ -26,11 +27,12 @@ def main():
 def process_file(filename):
     """Extract annual Wimbledon champion and nationality details from a supplied file and return them as a nested
     list."""
+    results = []
     with open(filename, 'r', encoding='utf-8-sig') as in_file:
         in_file.readline()  # Ignore header row
-        results = []
+        # Extract year, nationality, champion for each annual result
         for line in in_file:
-            result = line.strip().split(',')[:3]  # Extract year, nationality, champion for each annual result
+            result = line.strip().split(',')[:(INDEX_CHAMPION + 1)]
             results.append(result)
     return results
 
@@ -40,15 +42,15 @@ def store_champion_wins(results):
     champion_to_wins = {}
     for result in results:
         try:
-            champion_to_wins[result[2]] += 1
+            champion_to_wins[result[INDEX_CHAMPION]] += 1
         except KeyError:
-            champion_to_wins[result[2]] = 1
+            champion_to_wins[result[INDEX_CHAMPION]] = 1
     return champion_to_wins
 
 
 def store_countries(results):
     """Store the countries of the champions in a set."""
-    countries = {result[1] for result in results}
+    countries = {result[INDEX_COUNTRY] for result in results}
     return countries
 
 
