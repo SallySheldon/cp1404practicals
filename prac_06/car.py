84ffa0e4e42c8ade4128356cbc1c86ae03b2b45d
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-07-16 11:20:36 +1000

    Complete code for used_cars.py program and Car class in car.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,16 +1,22 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""CP1404 Practical 06 - Car class example."""
 
 
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, used to refer to a Car object
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
+
+    def __str__(self):
+        """Return string representation of data in a Car object."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
