5743eae0f4f338e0753c91ff135f245bcf18caec
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-07-16 12:16:11 +1000

    Change __init__ comment to recognise self.reflection is Boolean variable (after reviewing sample solution)

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,14 +5,14 @@ Define a class for a programming language
 
 
 class ProgrammingLanguage:
-    """Represent a Programming Language object."""
+    """Represent information about a programming language."""
 
     def __init__(self, name="", typing="", reflection="", year=0):
-        """Initialise a programming language object.
+        """Initialise a ProgrammingLanguage object.
 
         name: string, reference name for object
         typing: string, either Static or Dynamic typing
-        reflection:  string, either Yes or No
+        reflection:  Boolean, either True or False
         year: integer, year language first appeared
         """
         self.name = name
@@ -21,9 +21,9 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
-        """Return a string representation of a programming language object."""
+        """Return a string representation of a ProgrammingLanguage object."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
-        """Return True/False if programming language object is dynamically typed or not."""
+        """Determine whether a ProgrammingLanguage object is dynamically typed or not."""
         return self.typing == "Dynamic"
