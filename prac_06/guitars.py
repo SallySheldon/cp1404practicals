686379f59d813c715ad9162d2a9f3d97c9933076
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-07-16 14:34:18 +1000

    Include check in guitars.py for whether guitars list is empty (after reviewing sample solution)

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -22,18 +22,20 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added.")
         name = input("Name: ")
-    guitars.sort()
 
-    # Calculate alignments for display formatting
-    max_number = len(str(len(guitars)))
-    max_name = max((len(guitar.name) for guitar in guitars))
-    max_cost = max((len(str(guitar.cost)) for guitar in guitars)) + 3  # Add 3 for $ , and . characters
-
-    print("These are my guitars:")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = "(vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i:>{max_number}}: {guitar.name:>{max_name}} ({guitar.year}), worth $"
-              f"{guitar.cost:>{max_cost},.2f} {vintage_string}")
+    if guitars:  # First check whether the list has contents
+        guitars.sort()
+        # Calculate alignments for display formatting
+        max_number = len(str(len(guitars)))
+        max_name = max((len(guitar.name) for guitar in guitars))
+        max_cost = max((len(str(guitar.cost)) for guitar in guitars)) + 3  # Add 3 for $ , and . characters
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = "(vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i:>{max_number}}: {guitar.name:>{max_name}} ({guitar.year}), worth $"
+                  f"{guitar.cost:>{max_cost},.2f} {vintage_string}")
+    else:
+        print("As yet, I have no guitars.")
 
 
 main()
