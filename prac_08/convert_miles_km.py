a12ec1f542033662f5d4b2b85ee83add4de0f787
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-07-29 19:03:34 +1000

    Update Convert Miles to Km App docstrings and comments

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,7 +4,7 @@ Miles to Kilometres converter
 Author: Sally Sheldon
 
 Estimated time to complete: 45 mins
-Actual time to complete:
+Actual time to complete: 80 mins
 """
 
 from kivy.app import App
@@ -26,15 +26,15 @@ class ConvertMilesToKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert_miles_to_km(self, text):
+    def handle_convert_miles_to_km(self, miles_input):
         """Handle conversion of miles to km; output km to output_label widget."""
         # Convert miles_input text to a number
-        miles = self.convert_text_to_number(text)
+        miles = self.convert_text_to_number(miles_input)
         # Calculate conversion and display km in output_label
         self.km_output = str(miles * KM_PER_MILE)
 
     def handle_increment(self, increment):
-        """Handle increment of miles_input by increasing or decreasing miles_input by 1."""
+        """Handle up/down button press by increasing/decreasing miles_input by 1."""
         # Convert miles_input text to a number
         miles = self.convert_text_to_number(self.root.ids.miles_input.text)
         # Increment miles up or down
