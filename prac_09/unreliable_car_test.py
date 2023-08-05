d9ea1c3c47a98facb69f526864fd103b272a4110
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-08-05 18:34:20 +1000

    Improve test code for UnreliableCar class testing

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -8,11 +8,16 @@ from unreliable_car import UnreliableCar
 
 def main():
     """Test drive an Unreliable Car object to check UnreliableCar class methods."""
-    my_dodgy_car = UnreliableCar(name="Dodge 1", fuel=100, reliability=80)
-    print(my_dodgy_car)
-    for i in range(0, 10):  # Test drive the car (5km) 10 times
-        my_dodgy_car.drive(5)
-        print(my_dodgy_car)
+    my_ok_car = UnreliableCar(name="OK car", fuel=100, reliability=80)
+    my_bad_car = UnreliableCar(name="Bad car", fuel=100, reliability=30)
+    print(f"Test driving {my_ok_car.name} and {my_bad_car.name} for 5km, 10 times:")
+    for i in range(0, 10):
+        print(f"Attempt {i}:")
+        print(f"{my_ok_car.name} drove {my_ok_car.drive(5)} km")
+        print(f"{my_bad_car.name} drove {my_bad_car.drive(5)} km")
+    print("The final state of both cars is:")
+    print(my_ok_car)
+    print(my_bad_car)
 
 
 main()
